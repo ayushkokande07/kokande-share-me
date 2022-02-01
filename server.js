5a995f6ddddaeb21dab8418963ef14521d2ce5c7
unknown <you@example.com>
2022-02-02 02:08:33 +0530

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.json());
+const path = require('path');
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use('/api/files',require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download',require('./routes/download'));
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
